49cc1ce2dfa187a994ada76c128b48983e3d6535
MichelleGri <michelle0g@hotmail.com>
2022-06-01 20:27:04 +0000

    Change html for game-area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
+// 'use strict';
+
+// Wait for the DOM to finish loading before running the Gamepad
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let buttons = document.getElementsByTagName("button");
+
+//     let images = document.getElementsByTagName("img");
+
+//     for (let images of images) {
+//         images.addEventListener("click", function () {
+//             if (this.getAttribute("data-type") === "img") {
+//                 alert("You clicked on an Image!");
+//                 else
+//             }
+//         })
+//     }
+// })
+
+
+
+// const cardsArray = [
+//     document.getElementsByClassName('game-card')
+// ]
+// console.log(cardsArray);
+
+const cardsArray = [];
+
+cardsArray.push('assets/images/augustin.jpeg');
+cardsArray.push('assets/images/alma.jpeg');
+cardsArray.push('assets/images/antonio.jpeg');
+cardsArray.push('assets/images/bruno.jpeg');
+
+// console.log(cardsArray);
+
+const randomCards = cardsArray[Math.floor(Math.random() * cardsArray.length)];
+
+console.log(randomCards);
+
+
+// for (let i = 0; i < cardsArray.length; i++) {
+//     console.log(cardsArray);
+// }
+
+
+
 function runGame() {
 
 }
