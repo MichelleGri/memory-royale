6402de9012683d0711c0ba7fef2a25b85c7fe887
MichelleGri <michelle0g@hotmail.com>
2022-06-08 20:43:48 +0000

    Add comments and docstrings to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", startGame);
 
 /* Set/reset all game parameters to their initial state. */
 function startGame() {
+
     // initial variable values
     score = 10;
     highscore;
@@ -29,14 +30,19 @@ function startGame() {
     displayMessage('🤩 Start playing!');
     matches = 0;
 
+    // unflip any flipped over cards
     for (let card of cards) {
         card.classList.remove('flip');
     };
 
+    // select 'score' class from DOM to store game score
     document.querySelector('.score').textContent = score;
 
+    // function to flip over game cards
     cards.forEach(card => card.addEventListener('click', flipCard));
 
+    /* shuffle cards to randomize game board
+     * add timeout to ensure all cards are flipped over before shuffling */
     setTimeout(() => {
         shuffle();
     }, 500);
@@ -53,13 +59,13 @@ function shuffle() {
     });
 };
 
-// function to decrease score after every incorrect macth
+// function to decrease score after every incorrect match
 function decreaseScore() {
     score--;
     document.querySelector('.score').textContent = score;
 };
 
-// function for clicking cards to flip over
+// function for clicking on cards to flip over
 function flipCard() {
     if (disableFlipping) return;
     if (this === card1) return;
@@ -73,9 +79,18 @@ function flipCard() {
     checkMatch();
 };
 
-// function to check if cards match
+/* main game play functionality:
+ * function to check if cards match, 
+ * do not match, 
+ * messages to display - timeout added to displayMessage to ensure it is displayed after two cards have completed flipping over
+ * decrease score, and 
+ * add highscore */
 function checkMatch() {
+    // variable to check if cards match - use of character dataset added in index.html
     let correctMatch = card1.dataset.character === card2.dataset.character;
+
+    // if cards do not match:
+    // score decreases by 1
     if (score > 0) {
         if (!correctMatch) {
             flipOverCards();
@@ -84,28 +99,38 @@ function checkMatch() {
             }, 500)
             decreaseScore();
         }
+
+        // if cards match:
+        // cards to remain flipped over
         if (correctMatch) {
             remainFlipped();
             setTimeout(() => {
                 displayMessage(`🎉 That's a match!`);
             }, 500)
+
+            // number of matches increases by 1
+            // game is finished when all cards are matched and flipped over
             matches++;
             if ((matches * 2 === gameCards.length)) {
                 setTimeout(() => {
                     displayMessage('✨ You won the game!');
                 }, 500)
 
+                // when game ends, if current score is higher than highscore, current score becomes the highscore
                 if (score > highscore) {
                     highscore = score;
                     document.querySelector('.highscore').textContent = highscore;
                 }
             };
         };
+
+        // if score decreases to 0, player does not win, and the game is over
         if (score === 0) {
             setTimeout(() => {
                 displayMessage('💥 Game Over!');
             }, 500)
 
+            // if game is over, the game starts again after 5000 milliseconds
             setTimeout(() => {
                 startGame();
             }, 5000)
@@ -120,7 +145,8 @@ function remainFlipped() {
     nextTurn();
 };
 
-// function for cards to flip over if they do not match
+// function for cards to unflip if they do not match
+// timeout set for cards to unflip after 3000 milliseconds to allow player to view and memorise the cards' positions
 function flipOverCards() {
     disableFlipping = true;
     setTimeout(() => {
